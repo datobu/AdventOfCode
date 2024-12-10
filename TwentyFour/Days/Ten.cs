99ec0e4f502395fd0885570b478fb114b59233e6
David <github@datobu.de>
2024-12-10 09:08:52 +0100

    still a bug

--- TwentyFour/Days/Ten.cs
+++ TwentyFour/Days/Ten.cs
@@ -2,9 +2,24 @@
 
 internal class Ten
 {
+    private readonly List<Trailhead> _trailheads = [];
     private int _numberOfRows = 0;
     private int _numberOfColumns = 0;
     private int[,] _matrix = null!;
+
+    public class Coordinate(int row, int col)
+    {
+        public int Row { get; private set; } = row;
+
+        public int Column { get; private set; } = col;
+    }
+
+    public class Trailhead(Coordinate ownPlace)
+    {
+        public Coordinate OwnPlace { get; set; } = ownPlace;
+
+        public List<Coordinate> StartPoints { get; private set; } = [];
+    }
 
     public int Run()
     {
@@ -36,76 +51,115 @@ internal class Ten
             x++;
         }
 
-        int sum = 0;
-
         for (int row = 0; row < _numberOfRows; row++)
         {
             for (int col = 0; col < _numberOfColumns; col++)
             {
                 if (_matrix[row, col] == 0)
                 {
-                    sum += FollowPath(row, col, 1, sum);
+                    CountTrailheads(new Coordinate(row, col), new Coordinate(row, col), 1);
                 }
             }
+        }
+
+        int sum = 0;
+
+        foreach (var trailhead in _trailheads)
+        {
+            sum += trailhead.StartPoints.Count;
         }
 
         return sum;
     }
 
-    private int FollowPath(int row, int col, int nextStep, int sum)
+    private void CountTrailheads(Coordinate start, Coordinate current, int nextStep)
     {
-        if (row - 1 >= 0)
+        Coordinate next = new Coordinate(current.Row + 1, current.Column);
+
+        if (next.Row < _numberOfRows && next.Row >= 0)
         {
-            if (_matrix[row - 1, col] == nextStep)
+            if (_matrix[next.Row, current.Column] == nextStep)
             {
                 if (nextStep == 9)
                 {
-                    return 1;
+                    AddTrailheadAndStartPoints(start, next);
+
+                    return;
                 }
 
-                sum += FollowPath(row - 1, col, nextStep + 1, sum);
+                CountTrailheads(start, new Coordinate(next.Row, current.Column), nextStep + 1);
             }
         }
 
-        if (row + 1 < _numberOfRows)
+        next = new Coordinate(current.Row - 1, current.Column);
+
+        if (next.Row < _numberOfRows && next.Row >= 0)
         {
-            if (_matrix[row + 1, col] == nextStep)
+            if (_matrix[next.Row, current.Column] == nextStep)
             {
                 if (nextStep == 9)
                 {
-                    return 1;
+                    AddTrailheadAndStartPoints(start, next);
+
+                    return;
                 }
 
-                sum += FollowPath(row + 1, col, nextStep + 1, sum);
+                CountTrailheads(start, new Coordinate(next.Row, current.Column), nextStep + 1);
             }
         }
 
-        if (col - 1 >= 0)
+        next = new Coordinate(current.Row, current.Column + 1);
+
+        if (next.Column < _numberOfColumns && next.Column >= 0)
         {
-            if (_matrix[row, col - 1] == nextStep)
+            if (_matrix[current.Row, next.Column] == nextStep)
             {
                 if (nextStep == 9)
                 {
-                    return 1;
+                    AddTrailheadAndStartPoints(start, next);
+
+                    return;
                 }
 
-                sum += FollowPath(row, col - 1, nextStep + 1, sum);
+                CountTrailheads(start, new Coordinate(current.Row, next.Column), nextStep + 1);
             }
         }
 
-        if (col + 1 < _numberOfColumns)
+        next = new Coordinate(current.Row, current.Column - 1);
+
+        if (next.Column < _numberOfColumns && next.Column >= 0)
         {
-            if (_matrix[row, col + 1] == nextStep)
+            if (_matrix[current.Row, next.Column] == nextStep)
             {
                 if (nextStep == 9)
                 {
-                    return 1;
+                    AddTrailheadAndStartPoints(start, next);
+
+                    return;
                 }
 
-                sum += FollowPath(row, col + 1, nextStep + 1, sum);
+                CountTrailheads(start, new Coordinate(current.Row, next.Column), nextStep + 1);
             }
         }
+    }
 
-        return 0;
+    private void AddTrailheadAndStartPoints(Coordinate start, Coordinate current)
+    {
+        Trailhead? trailhead = _trailheads.SingleOrDefault(x => x.OwnPlace.Row == current.Row && x.OwnPlace.Column == current.Column);
+        if (trailhead == null)
+        {
+            AddTrailheadWithStart(start, current);
+        }
+        else if (!trailhead.StartPoints.Any(x => x.Row == start.Row && x.Column == start.Column))
+        {
+            trailhead.StartPoints.Add(start);
+        }
+    }
+
+    private void AddTrailheadWithStart(Coordinate start, Coordinate current)
+    {
+        var trailhead = new Trailhead(current);
+        trailhead.StartPoints.Add(start);
+        _trailheads.Add(trailhead);
     }
 }
