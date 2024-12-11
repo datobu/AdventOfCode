230d72a2dd9b07d4c8d4a0a6acbb21ef32e64f7a
David <github@datobu.de>
2024-12-11 22:08:24 +0100

    test

--- TwentyFour/Days/Eleven.cs
+++ TwentyFour/Days/Eleven.cs
@@ -2,53 +2,80 @@
 
 internal class Eleven
 {
-    public int Run()
-    {
-        return PartOne();
+    private readonly int _runCount = 9;
 
-        // PartTwo();
+    public long Run()
+    {
+        Dictionary<long, int> dict = [];
+
+        dict.Add(5910927, 1);
+        dict.Add(0, 1);
+        dict.Add(1, 1);
+        dict.Add(47, 1);
+        dict.Add(261223, 1);
+        dict.Add(94788, 1);
+        dict.Add(545, 1);
+        dict.Add(7771, 1);
+
+        // 5910927 0 1 47 261223 94788 545 7771
+
+        for (int i = 0; i < _runCount; i++)
+        {
+            dict = RunThroughDict(dict);
+        }
+
+        int sum = 0;
+
+        foreach (var i in dict)
+        {
+            sum += i.Value;
+        }
+
+        return sum;
     }
 
-    private int PartOne()
+    private Dictionary<long, int> RunThroughDict(Dictionary<long, int> dict)
     {
-        var input = File.ReadAllText("../../../Common/Inputs/DayEleven.txt");
+        Dictionary<long, int> newDict = [];
 
-        List<long> stones = input.Split(' ').Select(long.Parse).ToList();
-
-        for (int i = 0; i < 9; i++)
+        foreach (var entry in dict)
         {
-            List<long> stonesAfterBlink = [];
-
-            foreach (long stone in stones)
+            if (entry.Key == 0)
             {
-                string textStone = stone.ToString();
+                AddValueToDict(newDict, 1, entry.Value - 1);
+            }
+            else
+            {
+                if (entry.Key.ToString().Length % 2 == 0)
+                {
+                    (long left, long right) = Split(entry.Key.ToString());
 
-                if (stone == 0)
-                {
-                    stonesAfterBlink.Add(1);
-                }
-                else if (textStone.Length % 2 == 0)
-                {
-                    (long newStoneLeft, long newStoneRight) = Split(textStone);
-                    stonesAfterBlink.Add(newStoneLeft);
-                    stonesAfterBlink.Add(newStoneRight);
+                    int oldCount = dict[entry.Key] - 1;
+                    oldCount += newDict.TryGetValue(left, out int value) ? value : 0;
+                    AddValueToDict(newDict, left, oldCount);
+
+                    oldCount = dict[entry.Key] - 1;
+                    oldCount += newDict.TryGetValue(right, out value) ? value : 0;
+                    AddValueToDict(newDict, right, oldCount);
                 }
                 else
                 {
-                    stonesAfterBlink.Add(stone * 2024);
+                    AddValueToDict(newDict, entry.Key * 2024, entry.Value - 1);
                 }
             }
-
-            stones = stonesAfterBlink;
         }
 
-        foreach (long stone in stones)
+        return newDict;
+    }
+
+    private static void AddValueToDict(Dictionary<long, int> dict, long value, int oldCount)
+    {
+        if (!dict.ContainsKey(value))
         {
-            Console.Write($"{stone} ");
+            dict.Add(value, 0);
         }
 
-        Console.WriteLine(stones.Count);
-        return 0;
+        dict[value] = oldCount + 1;
     }
 
     private (long NewStoneLeft, long NewStoneRight) Split(string textStone)
