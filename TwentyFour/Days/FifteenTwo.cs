4ac9bd26653fb2f1a51b6fca79c76579233eea67
David <github@datobu.de>
2024-12-15 14:40:16 +0100

    Day 15 part 2 without help!

--- TwentyFour/Days/FifteenTwo.cs
+++ TwentyFour/Days/FifteenTwo.cs
@@ -4,7 +4,7 @@ public class FifteenTwo
 {
     public static class GameParameter
     {
-        public const string Path = "../../../Common/Inputs/DayFifteen-Example.txt";
+        public const string Path = "../../../Common/Inputs/DayFifteen.txt";
 
         // public const string Path = "../../../Common/Inputs/DayFifteen.txt";
     }
@@ -13,6 +13,8 @@ public class FifteenTwo
     private int _height;
     private string _moveOrders = string.Empty;
     private char[,] _map = null!;
+
+    private int _i = 0;
 
     public int PartTwo()
     {
@@ -23,15 +25,18 @@ public class FifteenTwo
         Console.WriteLine("Init");
         PrintMap(_map);
 
-        int i = 0;
-
         foreach (char c in _moveOrders)
         {
             (y, x) = MakeStep(c, y, x);
-            Console.WriteLine();
-            Console.WriteLine($"{++i} / {_moveOrders.Length}: {c}");
-            PrintMap(_map);
+            _i++;
+
+            if (_i == 4758 || _i == 4759 || _i == 4760)
+            {
+                PrintMap(_map, c);
+            }
         }
+
+        PrintMap(_map);
 
         int sum = 0;
 
@@ -53,15 +58,15 @@ public class FifteenTwo
     {
         return stepChar switch
         {
-            '>' => MoveHorizontically(stepChar, y, x, +1),
-            '<' => MoveHorizontically(stepChar, y, x, -1),
-            '^' => MoveVertically(stepChar, y, x, -1, 0),
-            'v' => MoveVertically(stepChar, y, x, +1, 0),
+            '>' => MoveHorizontically(y, x, +1),
+            '<' => MoveHorizontically(y, x, -1),
+            '^' => MoveVertically(y, x, -1),
+            'v' => MoveVertically(y, x, +1),
             _ => throw new Exception(),
         };
     }
 
-    private (int Y, int X) MoveHorizontically(char stepChar, int y, int x, int xStep)
+    private (int Y, int X) MoveHorizontically(int y, int x, int xStep)
     {
         if (_map[y, x + xStep] == '#')
         {
@@ -83,8 +88,18 @@ public class FifteenTwo
                 {
                     _map[y, x] = '.';
                     _map[y, x + xStep] = '@';
-                    _map[y, x + lookStep - 1] = '[';
-                    _map[y, x + lookStep] = ']';
+                    for (int i = 2; i <= lookStep; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            _map[y, x + i] = '[';
+                        }
+                        else
+                        {
+                            _map[y, x + i] = ']';
+                        }
+                    }
+
                     return (y, x + xStep);
                 }
 
@@ -104,8 +119,19 @@ public class FifteenTwo
                 {
                     _map[y, x] = '.';
                     _map[y, x + xStep] = '@';
-                    _map[y, x + lookStep + 1] = ']';
-                    _map[y, x + lookStep] = '[';
+
+                    for (int i = -2; i >= lookStep; i--)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            _map[y, x + i] = ']';
+                        }
+                        else
+                        {
+                            _map[y, x + i] = '[';
+                        }
+                    }
+
                     return (y, x + xStep);
                 }
 
@@ -119,20 +145,116 @@ public class FifteenTwo
         throw new Exception();
     }
 
-    private (int Y, int X) MoveVertically(char stepChar, int y, int x, int yStep, int xStep)
+    private (int Y, int X) MoveVertically(int y, int x, int yStep)
     {
-        if (_map[y + yStep, x + xStep] == '#')
+        if (_map[y + yStep, x] == '#')
         {
             return (y, x);
         }
-        else if (_map[y + yStep, x + xStep] == '.')
+        else if (_map[y + yStep, x] == '.')
         {
-            _map[y + yStep, x + xStep] = '@';
+            _map[y + yStep, x] = '@';
             _map[y, x] = '.';
-            return (y + yStep, x + xStep);
+            return (y + yStep, x);
+        }
+        else
+        {
+            List<Tuple<int, int, char>> boxParts = [];
+
+            bool isLeft = true;
+            if (_map[y + yStep, x] == '[')
+            {
+                boxParts.Add(new Tuple<int, int, char>(y + yStep, x, '['));
+                boxParts.Add(new Tuple<int, int, char>(y + yStep, x + 1, ']'));
+            }
+            else if (_map[y + yStep, x] == ']')
+            {
+                isLeft = false;
+                boxParts.Add(new Tuple<int, int, char>(y + yStep, x - 1, '['));
+                boxParts.Add(new Tuple<int, int, char>(y + yStep, x, ']'));
+            }
+
+            bool movingIsPossible = MoveEverythingIfPossible(boxParts, y + yStep, yStep);
+
+            if (movingIsPossible)
+            {
+                _map[y + yStep, x] = '@';
+                _map[y, x] = '.';
+                if (isLeft)
+                {
+                    _map[y + yStep, x + 1] = '.';
+                }
+                else
+                {
+                    _map[y + yStep, x - 1] = '.';
+                }
+
+                return (y + yStep, x);
+            }
         }
 
         return (y, x);
+    }
+
+    private bool MoveEverythingIfPossible(List<Tuple<int, int, char>> boxParts, int y, int direction)
+    {
+        var currentYBoxParts = boxParts.Where(b => b.Item1 == y);
+        bool isBlocked = currentYBoxParts.Any(x => _map[y + direction, x.Item2] == '#');
+        if (isBlocked)
+        {
+            return false;
+        }
+
+        bool isFree = currentYBoxParts.All(x => _map[y + direction, x.Item2] == '.');
+        if (isFree)
+        {
+            MoveAll(boxParts, direction);
+            return true;
+        }
+
+        List<Tuple<int, int, char>> allBoxParts = new(boxParts);
+
+        foreach (var boxPart in currentYBoxParts)
+        {
+            if (_map[y + direction, boxPart.Item2] == '[')
+            {
+                if (!allBoxParts.Any(x => x.Item1 == y + direction && x.Item2 == boxPart.Item2))
+                {
+                    allBoxParts.Add(new Tuple<int, int, char>(y + direction, boxPart.Item2, '['));
+                    allBoxParts.Add(new Tuple<int, int, char>(y + direction, boxPart.Item2 + 1, ']'));
+                }
+            }
+            else if (_map[y + direction, boxPart.Item2] == ']')
+            {
+                if (!allBoxParts.Any(x => x.Item1 == y + direction && x.Item2 == boxPart.Item2))
+                {
+                    allBoxParts.Add(new Tuple<int, int, char>(y + direction, boxPart.Item2 - 1, '['));
+                    allBoxParts.Add(new Tuple<int, int, char>(y + direction, boxPart.Item2, ']'));
+                }
+            }
+        }
+
+        return MoveEverythingIfPossible(allBoxParts, y + direction, direction);
+    }
+
+    private void MoveAll(List<Tuple<int, int, char>> boxParts, int direction)
+    {
+        if (direction < 0)
+        {
+            foreach (var boxPart in boxParts.OrderBy(x => x.Item1))
+            {
+                _map[boxPart.Item1 + direction, boxPart.Item2] = boxPart.Item3;
+                _map[boxPart.Item1, boxPart.Item2] = '.';
+            }
+        }
+        else
+        {
+            foreach (var boxPart in boxParts.OrderByDescending(x => x.Item1))
+            {
+                _map[boxPart.Item1 + direction, boxPart.Item2] = boxPart.Item3;
+                _map[boxPart.Item1, boxPart.Item2] = '.';
+            }
+        }
     }
 
     private (int YStart, int XStart) GetStartingPosition()
@@ -207,13 +329,48 @@ public class FifteenTwo
         }
     }
 
-    private void PrintMap(char[,] map)
+    private void PrintMap(char[,] map, char c = ' ')
     {
-        for (int i = 0; i < _height; i++)
+        Console.WriteLine();
+        Console.WriteLine($"{_i} / {_moveOrders.Length}: {c}");
+
+        for (int i = -1; i < _height; i++)
         {
-            for (int j = 0; j < _width; j++)
+            for (int j = -1; j < _width; j++)
             {
-                Console.Write($"{map[i, j]} ");
+                if (i == -1)
+                {
+                    if (j == -1)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        string formattedNumber = j.ToString("D2");
+                        Console.Write($"{formattedNumber}");
+                    }
+                }
+                else
+                {
+                    if (j == -1)
+                    {
+                        string formattedNumber = i.ToString("D2");
+                        Console.Write($"{formattedNumber} ");
+                    }
+                    else
+                    {
+                        if (map[i, j] == '@')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write($"@ ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write($"{map[i, j]} ");
+                        }
+                    }
+                }
             }
 
             Console.WriteLine();
