624eebe770b2477de329c2fba17e4559b6277615
David <github@datobu.de>
2024-12-17 09:37:39 +0100

    klappt nicht...

--- TwentyFour/Days/Sixteen.cs
+++ TwentyFour/Days/Sixteen.cs
@@ -1,15 +1,213 @@
 ﻿namespace TwentyFour.Days;
 
-// I was stuck so I asked chat gpt...
+// Neuer Ansatz: Lösungsbaum - funktioniert mit beiden beispielen... aber in der echten welt schon wieder infinite loop...
 public class Sixteen
 {
-    private static readonly (int, int)[] Directions =
+    public enum Direction
     {
-        (0, 1), // Right
-        (1, 0), // Down
-        (0, -1), // Left
-        (-1, 0) // Up
-    };
+        North,
+        South,
+        East,
+        West,
+    }
+
+    public class PathNode(
+        List<PathNode> precedessors,
+        Direction direction,
+        int y,
+        int x,
+        char[,] map,
+        int score)
+    {
+        public List<PathNode> Precedessors { get; private set; } = precedessors;
+
+        public Direction Direction { get; private set; } = direction;
+
+        public int Y { get; private set; } = y;
+
+        public int X { get; private set; } = x;
+
+        public int Score { get; private set; } = score;
+
+        public PathNode? North { get; private set; }
+
+        public PathNode? South { get; private set; }
+
+        public PathNode? East { get; private set; }
+
+        public PathNode? West { get; private set; }
+
+        private readonly char[,] _map = map;
+
+        public bool IsFinalStep { get; private set; } = false;
+
+        public void GoNextStep()
+        {
+            if (Direction != Direction.South)
+            {
+                if (Direction == Direction.North)
+                {
+                    GoNorth(1);
+                }
+                else
+                {
+                    GoNorth(1001);
+                }
+            }
+
+            if (Direction != Direction.North)
+            {
+                if (Direction == Direction.South)
+                {
+                    GoSouth(1);
+                }
+                else
+                {
+                    GoSouth(1001);
+                }
+            }
+
+            if (Direction != Direction.West)
+            {
+                if (Direction == Direction.East)
+                {
+                    GoEast(1);
+                }
+                else
+                {
+                    GoEast(1001);
+                }
+            }
+
+            if (Direction != Direction.East)
+            {
+                if (Direction == Direction.West)
+                {
+                    GoWest(1);
+                }
+                else
+                {
+                    GoWest(1001);
+                }
+            }
+        }
+
+        private void GoNorth(int stepScore)
+        {
+            if (Precedessors.Any(pre => pre.X == X && pre.Y == Y - 1))
+            {
+                return;
+            }
+
+            switch (_map[Y - 1, X])
+            {
+                case 'E':
+                    IsFinalStep = true;
+                    Score += stepScore;
+                    var newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    North = new PathNode(newPrecedessors, Direction.North, Y - 1, X, _map, Score + stepScore);
+                    break;
+
+                case '.':
+                    newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    North = new PathNode(newPrecedessors, Direction.North, Y - 1, X, _map, Score + stepScore);
+                    break;
+            }
+        }
+
+        private void GoSouth(int stepScore)
+        {
+            if (Precedessors.Any(pre => pre.X == X && pre.Y == Y + 1))
+            {
+                return;
+            }
+
+            switch (_map[Y + 1, X])
+            {
+                case 'E':
+                    IsFinalStep = true;
+                    Score += stepScore;
+                    var newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    South = new PathNode(newPrecedessors, Direction.South, Y + 1, X, _map, Score + stepScore);
+                    break;
+
+                case '.':
+                    newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    South = new PathNode(newPrecedessors, Direction.South, Y + 1, X, _map, Score + stepScore);
+                    break;
+            }
+        }
+
+        private void GoEast(int stepScore)
+        {
+            if (Precedessors.Any(pre => pre.X == X + 1 && pre.Y == Y))
+            {
+                return;
+            }
+
+            switch (_map[Y, X + 1])
+            {
+                case 'E':
+                    IsFinalStep = true;
+                    Score += stepScore;
+                    var newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    East = new PathNode(newPrecedessors, Direction.East, Y, X + 1, _map, Score + stepScore);
+                    break;
+
+                case '.':
+                    newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    East = new PathNode(newPrecedessors, Direction.East, Y, X + 1, _map, Score + stepScore);
+                    break;
+            }
+        }
+
+        private void GoWest(int stepScore)
+        {
+            if (Precedessors.Any(pre => pre.X == X - 1 && pre.Y == Y))
+            {
+                return;
+            }
+
+            switch (_map[Y, X - 1])
+            {
+                case 'E':
+                    IsFinalStep = true;
+                    Score += stepScore;
+                    var newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    West = new PathNode(newPrecedessors, Direction.West, Y, X - 1, _map, Score + stepScore);
+                    break;
+
+                case '.':
+                    newPrecedessors = new List<PathNode>(Precedessors)
+                    {
+                        this
+                    };
+                    West = new PathNode(newPrecedessors, Direction.West, Y, X - 1, _map, Score + stepScore);
+                    break;
+            }
+        }
+    }
 
     public static class GameParameter
     {
@@ -24,93 +222,115 @@ public class Sixteen
     {
         Init();
 
-        int score = SolveMaze(_map);
+        (int y, int x) = GetStartingPosition();
 
-        return score;
+        var startNode = new PathNode(new List<PathNode>(), Direction.East, y, x, _map, 0);
+
+        List<PathNode> currentNodeList = [];
+        currentNodeList.Add(startNode);
+
+        currentNodeList.ForEach(node => node.GoNextStep());
+
+        int lowestScore = -1;
+
+        while (currentNodeList.Any(node => node.North != null || node.South != null || node.East != null || node.West != null))
+        {
+            List<PathNode> innerCurrentNodeList = [];
+
+            foreach (var node in currentNodeList)
+            {
+                if (node.North != null)
+                {
+                    if (node.IsFinalStep)
+                    {
+                        if (lowestScore == -1 || node.Score < lowestScore)
+                        {
+                            lowestScore = node.Score;
+                        }
+                    }
+
+                    innerCurrentNodeList.Add(node.North);
+                }
+
+                if (node.South != null)
+                {
+                    if (node.IsFinalStep)
+                    {
+                        if (lowestScore == -1 || node.Score < lowestScore)
+                        {
+                            lowestScore = node.Score;
+                        }
+                    }
+
+                    innerCurrentNodeList.Add(node.South);
+                }
+
+                if (node.East != null)
+                {
+                    if (node.IsFinalStep)
+                    {
+                        if (lowestScore == -1 || node.Score < lowestScore)
+                        {
+                            lowestScore = node.Score;
+                        }
+                    }
+
+                    innerCurrentNodeList.Add(node.East);
+                }
+
+                if (node.West != null)
+                {
+                    if (node.IsFinalStep)
+                    {
+                        if (lowestScore == -1 || node.Score < lowestScore)
+                        {
+                            lowestScore = node.Score;
+                        }
+                    }
+
+                    innerCurrentNodeList.Add(node.West);
+                }
+            }
+
+            innerCurrentNodeList.ForEach(node => node.GoNextStep());
+            currentNodeList = new List<PathNode>(innerCurrentNodeList);
+        }
+
+        foreach (var node in currentNodeList)
+        {
+            lowestScore = GetLowestScore(node, lowestScore);
+        }
+
+        return lowestScore;
     }
 
-    public int SolveMaze(char[,] maze)
+    private static int GetLowestScore(PathNode node, int lowestScore)
     {
-        _height = maze.GetLength(0);
-        _width = maze.GetLength(1);
-        (int, int) start = (-1, -1), end = (-1, -1);
-
-        // Locate start (S) and end (E) points
-        for (int r = 0; r < _height; r++)
+        if (node != null && node.IsFinalStep)
         {
-            for (int c = 0; c < _width; c++)
+            if (lowestScore == -1 || node.Score < lowestScore)
             {
-                if (maze[r, c] == 'S')
-                {
-                    start = (r, c);
-                }
+                lowestScore = node.Score;
+            }
+        }
 
-                if (maze[r, c] == 'E')
+        return lowestScore;
+    }
+
+    private (int YStart, int XStart) GetStartingPosition()
+    {
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                if (_map[y, x] == 'S')
                 {
-                    end = (r, c);
+                    return (y, x);
                 }
             }
         }
 
-        if (start == (-1, -1) || end == (-1, -1))
-        {
-            throw new ArgumentException("Maze must contain start 'S' and end 'E'.");
-        }
-
-        var queue = new Queue<(int Row, int Col, int Dir, int Steps, int Score)>();
-        var visited = new HashSet<(int, int, int)>();
-
-        // Initialize BFS with all possible starting directions
-        for (int i = 0; i < Directions.Length; i++)
-        {
-            var newRow = start.Item1 + Directions[i].Item1;
-            var newCol = start.Item2 + Directions[i].Item2;
-
-            if (maze[newRow, newCol] == '.')
-            {
-                if (i == 0)
-                {
-                    queue.Enqueue((newRow, newCol, i, 1, 1));
-                }
-                else
-                {
-                    queue.Enqueue((newRow, newCol, i, 1, 1001));
-                }
-
-                visited.Add((newRow, newCol, i));
-            }
-        }
-
-        int minScore = int.MaxValue;
-
-        while (queue.Count > 0)
-        {
-            var (row, col, dir, steps, score) = queue.Dequeue();
-
-            if ((row, col) == end)
-            {
-                minScore = Math.Min(minScore, score);
-                continue;
-            }
-
-            for (int i = 0; i < Directions.Length; i++)
-            {
-                var newRow = row + Directions[i].Item1;
-                var newCol = col + Directions[i].Item2;
-                int newScore = score + 1 + (i == dir ? 0 : 1000);
-
-                if (maze[newRow, newCol] == '.' || maze[newRow, newCol] == 'E')
-                {
-                    if (!visited.Contains((newRow, newCol, i)))
-                    {
-                        queue.Enqueue((newRow, newCol, i, steps + 1, newScore));
-                        visited.Add((newRow, newCol, i));
-                    }
-                }
-            }
-        }
-
-        return minScore;
+        throw new Exception();
     }
 
     private void Init()
@@ -137,19 +357,4 @@ public class Sixteen
             }
         }
     }
-
-    /* private void PrintMap(char[,] map)
-    {
-        for (int i = 0; i < _height; i++)
-        {
-            for (int j = 0; j < _width; j++)
-            {
-                Console.Write($"{map[i, j]} ");
-            }
-
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
-    }*/
 }
